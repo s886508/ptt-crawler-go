f03a652e8dc711edad28901773d1fe6fb3e5c3c6
Will WC Huang <s951553@gmail.com>
2021-07-03 14:34:16 +0800

    Enhance command line usage
    - Support 3 output option(files, elasticsearch, stdout), print to stdout as default.
    - Improve code readability.

--- cmd/ptt-crawler/main.go
+++ cmd/ptt-crawler/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
+	"github.com/s886508/ptt-crawler-go/pkg/article"
 	"github.com/s886508/ptt-crawler-go/pkg/crawler"
 	"github.com/s886508/ptt-crawler-go/pkg/storage"
 )
@@ -13,7 +15,10 @@ func main() {
 	board := flag.String("board", "", "board name to retrieve articles")
 	startPage := flag.Int64("start", 1, "start page to retrieve articles")
 	endPage := flag.Int64("end", 10, "end page to retrieve articles")
-	outputDir := flag.String("directory", "", "directory to save retrieved articles")
+	outputDir := flag.String("directory", "", "[optional] save retrieved articles to directory, "+
+		"print to stdout if none of output option is specified")
+	esServer := flag.String("elastic", "", "[optional] save retrieved articles to elasticsearch server, "+
+		"print to stdout if none of output option is specified")
 
 	flag.Parse()
 
@@ -23,35 +28,62 @@ func main() {
 	}
 
 	if *startPage <= 0 || *endPage <= 0 || *endPage < *startPage {
-		return
+		log.Fatal("invalid page numbers")
 	}
 
-	s := storage.ESStorage{}
-	err := s.Init("http://localhost:9200")
-	if err != nil {
-		return
+	var s *storage.ESStorage = nil
+	if len(*esServer) > 0 {
+		s = &storage.ESStorage{}
+		err := s.Init(*esServer)
+		if err != nil {
+			return
+		}
 	}
 
 	articles := crawler.GetArticles(*startPage, *endPage, *board)
 	for _, a := range articles {
 		// output as files
-		if len(*outputDir) > 0 {
-			filePath := fmt.Sprintf("%s/%s/%s.json", *outputDir, *board, a.Id)
-			err := a.Save(filePath, false)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+		saveToFile(*outputDir, *board, a)
 
 		// output to elasticsearch
-		doc, err := a.Dump()
-		if err != nil {
-			return
-		}
+		saveToElastic(s, *board, a)
 
-		err = s.AddDocument(a.Id, doc, *board)
-		if err != nil {
-			return
-		}
+		// output to stdout if no ouput option is specified
+		printToScreen(*outputDir, *esServer, a)
 	}
+}
+
+func saveToFile(dir string, board string, article *article.Article) {
+	if len(dir) == 0 {
+		return
+	}
+
+	filePath := fmt.Sprintf("%s/%s/%s.json", dir, board, article.Id)
+	err := article.Save(filePath, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func saveToElastic(s *storage.ESStorage, board string, article *article.Article) {
+	if s == nil {
+		return
+	}
+	content, err := article.Dump()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = s.AddDocument(article.Id, content, board)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printToScreen(dir string, elastic string, article *article.Article) {
+	if len(dir) > 0 || len(elastic) > 0 {
+		return
+	}
+
+	fmt.Printf("%s\n%v\n%s\n\n", strings.Repeat("%", 80), article, strings.Repeat("%", 80))
 }
